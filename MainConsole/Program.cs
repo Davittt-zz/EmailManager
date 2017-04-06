ed23beb269bbb558395d8765b6dcb7f5768559e9
Davittt <ddonari@gmail.com>
2017-04-06 13:21:11 -0300

    Main console to make test

---
+++ MainConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using EmailManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			EmailProcessor.ProcessEmail("samplefile");
+		}
+	}
+}
